f4ee707b5f7745b730c510691ba418653b510a3f
ramsterhad <ramsterhad@gmail.com>
2021-03-13 14:04:07 +0000

    Added mainly tests for the method requestPost

--- Application/Api/Danbooru/Exception/PostResponseException.php
+++ Application/Api/Danbooru/Exception/PostResponseException.php
@@ -6,5 +6,9 @@ namespace Ramsterhad\DeepDanbooruTagAssist\Application\Api\Danbooru\Exception;
 
 class PostResponseException extends \Exception
 {
-
+    const CODE_INVALID_JSON = 100;
+    const CODE_JSON_CONTAINS_NO_ITEM = 101;
+    const CODE_JSON_CONTAINS_MORE_THAN_ONE_ITEM = 102;
+    const CODE_JSON_ITEM_IS_NOT_OBJECT = 103;
+    const CODE_JSON_ITEM_IS_MISSING_PROPERTIES = 104;
 }
