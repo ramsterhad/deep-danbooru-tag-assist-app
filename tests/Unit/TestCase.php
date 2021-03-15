3cca03e3e35b8b09587ee8bec8cea480c3702cbf
ramsterhad <ramsterhad@gmail.com>
2021-03-15 22:55:20 +0000

    Removed old code.

--- tests/Unit/TestCase.php
+++ tests/Unit/TestCase.php
@@ -6,31 +6,5 @@ namespace Ramsterhad\DeepDanbooruTagAssist\Tests\Unit;
 
 class TestCase extends \PHPUnit\Framework\TestCase
 {
-    /**
-     * @link https://stackoverflow.com/a/2798203
-     *
-     * @param string $class
-     * @param string $name
-     * @return \ReflectionMethod
-     * @throws \ReflectionException
-     */
-    /*
-    protected static function getMethod(string $class, string $name): \ReflectionMethod
-    {
-        $class = new \ReflectionClass($class);
-        $method = $class->getMethod($name);
-        $method->setAccessible(true);
-        return $method;
-    }
-    */
-    /*
-    protected static function getProperty(string $class, string $name): \ReflectionMethod
-    {
-        $class = new \ReflectionClass($class);
-        $property = $class->getProperty($name);
-        $property->setAccessible(true);
-        //var_dump($reflectionProperty->getValue(new Foo));
-        return $property;
-    }
-    */
+
 }
