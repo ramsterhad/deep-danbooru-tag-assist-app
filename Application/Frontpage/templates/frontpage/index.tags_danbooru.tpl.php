9618f87fbebe3246a6c1717eb6e076c8f9307cde
ramsterhad <ramsterhad@gmail.com>
2021-06-27 23:00:31 +0200

    Linked the danbooru tags to the wiki pages.

--- Application/Frontpage/templates/frontpage/index.tags_danbooru.tpl.php
+++ Application/Frontpage/templates/frontpage/index.tags_danbooru.tpl.php
@@ -2,7 +2,11 @@
     <div class="col s2">Danbooru tags</div>
     <div class="col s10">
         <?php foreach ($response->get('danbooru')->getPost()->getTagCollection()->getTags() as $tag) : ?>
-            <span class="tag"><?php echo $response->getController()->tagsCssClassHelperColoredDanbooruTags($tag); ?></span>
+            <span class="tag">
+                <a href="https://danbooru.donmai.us/wiki_pages/<?php echo $tag->getName(); ?>" target="_blank">
+                    <?php echo $response->getController()->tagsCssClassHelperColoredDanbooruTags($tag); ?>
+                </a>
+            </span>
         <?php endforeach; ?>
     </div>
 </div>
