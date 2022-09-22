dae170c5927b2ec9a179e815c9ebbb2668359f60
ramsterhad <ramsterhad@gmail.com>
2022-09-22 15:20:30 +0200

    Removed bootstrap

--- public/index.php
+++ public/index.php
@@ -4,7 +4,6 @@ namespace Ramsterhad\DeepDanbooruTagAssist;
 
 use Ramsterhad\DeepDanbooruTagAssist\Application\Kernel;
 
-require_once '../bootstrap.php';
 define('BASE_PATH', __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR);
 
 require_once BASE_PATH . 'vendor/autoload.php';
